75c73d91e564da9f379e23188d0e41ed698269bd
Caio <caioosalencar@gmail.com>
2025-11-02 08:32:32 -0400

    Add eslint config to convert errors to warnings

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-mixed-operators': 'warn'
+  }
+}
